9ea313859cda96da73e8e262523637254451dd5f
Khánh Băng <trieukhanhbang123@gmail.com>
2023-02-10 11:07:28 +0900

    Add project files.

---
+++ Timer/Timer.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Timer
+{
+    partial class Timer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.Label();
+            this.dtpSetTimer = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hiểnThịToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = true;
+            this.toolStripStatusLabel1.Location = new System.Drawing.Point(80, 44);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(35, 13);
+            this.toolStripStatusLabel1.TabIndex = 0;
+            this.toolStripStatusLabel1.Text = "0 giây";
+            // 
+            // dtpSetTimer
+            // 
+            this.dtpSetTimer.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpSetTimer.Location = new System.Drawing.Point(12, 12);
+            this.dtpSetTimer.Name = "dtpSetTimer";
+            this.dtpSetTimer.Size = new System.Drawing.Size(200, 20);
+            this.dtpSetTimer.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(218, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Đếm ngược:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hiểnThịToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // hiểnThịToolStripMenuItem
+            // 
+            this.hiểnThịToolStripMenuItem.Name = "hiểnThịToolStripMenuItem";
+            this.hiểnThịToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hiểnThịToolStripMenuItem.Text = "Hiển thị";
+            this.hiểnThịToolStripMenuItem.Click += new System.EventHandler(this.hiểnThịToolStripMenuItem_Click);
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 112);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dtpSetTimer);
+            this.Controls.Add(this.toolStripStatusLabel1);
+            this.Name = "Timer";
+            this.Text = "Timer";
+            this.Resize += new System.EventHandler(this.Timer_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label toolStripStatusLabel1;
+        private System.Windows.Forms.DateTimePicker dtpSetTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hiểnThịToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+    }
+}
+
---
+++ Timer/Timer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Timer : Form
+    {
+        public Timer()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            notifyIcon1.Icon = new Icon("icon.ico");
+        }
+
+        DateTime start;
+        System.Timers.Timer t;
+        long s;
+
+        public void CountDown(long seconds, DateTime slt)
+        {
+            start = slt;
+            s = seconds;
+            t.Start();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            long remainingSeconds = (long)(s - (DateTime.Now - start).TotalSeconds);
+            if (remainingSeconds <= 0)
+            {
+                t.Stop();
+                this.BeginInvoke((Action)(() => toolStripStatusLabel1.Text = "Xong !"));
+                MessageBox.Show("Đến giờ tắt máy !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                return;
+            }
+            this.BeginInvoke((Action)(() => toolStripStatusLabel1.Text = string.Format("{0} giây...", remainingSeconds)));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (t != null && t.Enabled)
+            {
+                t.Dispose();
+            }
+
+            t = new System.Timers.Timer { Interval = 1000 };
+            t.Elapsed += Tick;
+            CountDown(10, DateTime.Parse(dtpSetTimer.Value.ToString()));
+            toolStripStatusLabel1.Text = dtpSetTimer.Value.ToString();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Timer_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized) this.Hide();
+            else this.Show();
+        }
+
+        private void hiểnThịToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
